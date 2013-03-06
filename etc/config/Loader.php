1baf17249e418817775d5a36ef6037c84e762109
Alexis Galbourdin <i@alexisg.net>
2013-03-06 12:03:28 +0100

    Updated default config samples.

--- etc/config/Loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-\Agl\Core\ConfigLoader::add(
-	'more/locale',
-	array(
-		'config' => __DIR__ . DIRECTORY_SEPARATOR . 'config.json',
-		'events' => __DIR__ . DIRECTORY_SEPARATOR . 'events.json',
-		'locale' => realpath(__DIR__ . '/../' . DIRECTORY_SEPARATOR . 'locale') . DIRECTORY_SEPARATOR
-	)
-);
